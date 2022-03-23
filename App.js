7dec128e8c841580c415e40c0a431c83ee9a15ed
Carolynn17 <carolineapio17@gmail.com>
2022-03-23 13:06:14 +0300

    first commit

--- App.js
+++ App.js
@@ -24,3 +24,16 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+
+// export default function App() {
+//   return (
+//     <View>
+//       <Text>App</Text>
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({})
